d8c77c2ef23ef06436140503b4215ae40706ec85
Letimmey81 <tim.wethekam@tuta.io>
2023-07-26 01:05:06 +0200

    Added and tested basic functionality

--- main.lua
+++ main.lua
@@ -1,16 +1,31 @@
 local CombatLogMinion_EventFrame = CreateFrame("Frame");
+local PlayerName, PlayerRealm = UnitName("player");
 
 CombatLogMinion_EventFrame:RegisterEvent("CHALLENGE_MODE_START");
 CombatLogMinion_EventFrame:RegisterEvent("CHALLENGE_MODE_COMPLETED");
+CombatLogMinion_EventFrame:RegisterEvent("CHALLENGE_MODE_RESET");
 
-CombatLogMinion_EventFrame:SetScript("OnEvent", function(self, event, arg1)
+CombatLogMinion_EventFrame:SetScript("OnEvent", function(_, event, __)
     --Start logging on mythic+ dungeon start
     if event == "CHALLENGE_MODE_START" then
-        SendChatMessage("/combatlog", "SAY")
+        if (LoggingCombat()) then
+            LoggingCombat(0);
+        end
+
+        LoggingCombat(1);
+        SendChatMessage("Starting advanced combat logging!", "PARTY");
+        SendChatMessage("Good look and have fun!", "PARTY");
     end
 
-    --Stop logging on mythic+ dungeon start
+    --Stop logging on mythic+ dungeon completed
     if event == "CHALLENGE_MODE_COMPLETED" then
-        SendChatMessage("/combatlog", "SAY")
+        LoggingCombat(0);
+        SendChatMessage("Logging advanced combat has ended.", "PARTY")
+    end
+
+    --Stop logging on mythic+ dungeon reset
+    if event == "CHALLENGE_MODE_RESET" then
+        LoggingCombat(0);
+        SendChatMessage("Logging advanced combat has ended.", "PARTY")
     end
 end);
